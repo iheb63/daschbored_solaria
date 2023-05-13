2818d1737076f04ce3fe5e7b9a6c1da178c964a5
iheb <130716061+iheb63@users.noreply.github.com>
2023-05-13 19:34:43 +0100

    Update _🏩_solaria_KPI.py

--- _🏩_solaria_KPI.py
+++ _🏩_solaria_KPI.py
@@ -20,7 +20,7 @@ st.set_page_config(
 
 st.markdown("<h1 style=' color: rgb(0, 255, 255); font-size: 100px; text-align: center;'>-🌞solaria KPI-</h1>", unsafe_allow_html=True)
 
-logo =  Image.open("C:/Users/ihebt/OneDrive/Bureau/daschbored solaria/logo.png",mode="png")
+logo =  Image.open("C:/Users/ihebt/OneDrive/Bureau/daschbored solaria/logo.png",mode="RGB")
 st.sidebar.success("select a page    :arrow_up:")
 st.sidebar.image(logo)
 #____________music-app___________________
